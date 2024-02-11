c8fa8d9cf590b954b89343ef46bdb2e6dfdc3ac8
Ruslan Shulga <nycruslan@Ruslans-MacBook-Pro.local>
2024-02-11 14:02:59 -0500

    more development

--- src/components/JsonViewer.tsx
+++ src/components/JsonViewer.tsx
@@ -4,76 +4,8 @@ import { PrimitiveValue } from './PrimitiveValue';
 import { useCollapsible } from '../hooks';
 import { isCollapsible, getBrackets } from '../utils';
 import styles from '../styles.module.scss';
-import type { JsonNodeProps, JsonViewerProps, Primitive } from '../types';
-
-const JsonNode = memo(({ name, value }: JsonNodeProps): ReactElement => {
-  const { collapsed, toggleCollapse } = useCollapsible();
-  const collapsible = isCollapsible(value);
-  const [openingBracket, closingBracket] = getBrackets(value);
-
-  // Determine the appropriate class for the key based on collapsibility
-  const keyClass = collapsible
-    ? `${styles.key} ${styles.collapsible}`
-    : `${styles.key} ${styles.primitive}`;
-
-  return (
-    <div className={styles.node}>
-      {collapsible ? (
-        <div>
-          <span
-            onClick={toggleCollapse}
-            className={keyClass} // Use dynamic class name
-          >
-            <CollapsibleIndicator collapsed={collapsed} />
-            <span>"{name}": </span>
-            {collapsed ? (
-              <>
-                {openingBracket}
-                {typeof value === 'object' &&
-                value !== null &&
-                Object.keys(value).length ? (
-                  <span className={styles.dots}>...</span>
-                ) : (
-                  ''
-                )}
-                {closingBracket}
-              </>
-            ) : (
-              openingBracket
-            )}
-          </span>
-          {!collapsed && (
-            <div>
-              <div className={styles.content}>
-                {Array.isArray(value)
-                  ? value.map((item, index) => (
-                      <JsonNode
-                        key={index.toString()}
-                        name={index.toString()}
-                        value={item}
-                      />
-                    ))
-                  : typeof value === 'object' && value !== null
-                  ? Object.entries(value).map(([key, val]) => (
-                      <JsonNode key={key} name={key} value={val} />
-                    ))
-                  : null}
-              </div>
-              <span className={styles.key} onClick={toggleCollapse}>
-                {closingBracket}
-              </span>
-            </div>
-          )}
-        </div>
-      ) : (
-        <>
-          <span className={keyClass}>"{name}": </span>{' '}
-          <PrimitiveValue value={value as Primitive} />
-        </>
-      )}
-    </div>
-  );
-});
+import type { JsonViewerProps, Primitive } from '../types';
+import { JsonNode } from './JsonNode';
 
 const JsonViewer = memo(
   ({ data, rootName = 'root' }: JsonViewerProps): ReactElement => {
