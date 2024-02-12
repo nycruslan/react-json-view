10694df880ae26968a5742dc42775a5db4b45b0c
Ruslan Shulga <nycruslan@Ruslans-MacBook-Pro.local>
2024-02-11 22:27:31 -0500

    more development

--- src/components/JsonViewer.tsx
+++ src/components/JsonViewer.tsx
@@ -1,18 +1,12 @@
-import { KeyboardEvent, memo, ReactElement } from 'react';
-import { CollapsibleIndicator } from './CollapsibleIndicator';
-import { PrimitiveValue } from './PrimitiveValue';
-import { JsonNode } from './JsonNode';
-import { useCollapsible } from '../hooks';
-import {
-  isCollapsible,
-  getBrackets,
-  getKeyClass,
-  handleKeyDown,
-} from '../utils';
-import styles from '../styles.module.scss';
-import type { JsonViewerProps, Primitive } from '../types';
-import { CopyButton } from './CopyButton';
+// Import the necessary dependencies
+import { memo, ReactElement } from 'react';
 
+import type { JsonViewerProps } from '../types';
+
+import { JsonViewerContent } from './JsonViewerContent';
+import { CollapsibleProvider } from '../context';
+
+// Define the JsonViewer component
 const JsonViewer = memo(
   ({
     data,
@@ -21,99 +15,16 @@ const JsonViewer = memo(
     expandLevel = 0,
     onCopy,
   }: JsonViewerProps): ReactElement => {
-    const initialCollapsed = expandLevel < 1;
-    const { collapsed, toggleCollapse } = useCollapsible(
-      initialCollapsed,
-      `jsonViewer-${rootName}`
-    );
-    const collapsible = isCollapsible(data);
-    const [openingBracket, closingBracket] = getBrackets(data);
-    const keyClass = getKeyClass(collapsible);
-
-    if (!data) {
-      return <p>No data to show</p>;
-    }
-
     return (
-      <div style={style} className={`${styles.viewer} ${styles.node}`}>
-        {collapsible ? (
-          <>
-            <span
-              tabIndex={0}
-              role='button'
-              aria-expanded={!collapsed}
-              aria-label={`${rootName} expandable`}
-              onClick={toggleCollapse}
-              onKeyDown={(e: KeyboardEvent<HTMLDivElement>) =>
-                handleKeyDown(e, toggleCollapse)
-              }
-              className={keyClass}
-            >
-              <CollapsibleIndicator collapsed={collapsed} />
-              {rootName}:{' '}
-              {collapsed ? (
-                <>
-                  {openingBracket}
-                  {typeof data === 'object' &&
-                  data !== null &&
-                  Object.keys(data).length ? (
-                    <span className={styles.dots}>...</span>
-                  ) : null}
-                  {closingBracket}
-                </>
-              ) : (
-                openingBracket
-              )}
-            </span>
-            {onCopy && (
-              <CopyButton
-                handleCopy={() => {
-                  const copyInfo = { keys: [rootName], value: data };
-                  onCopy(copyInfo);
-                }}
-              />
-            )}
-            {!collapsed && (
-              <div
-                className={styles.content}
-                aria-label={`${rootName} content`}
-              >
-                {typeof data === 'object' && data !== null
-                  ? Object.entries(data).map(([key, value]) => (
-                      <JsonNode
-                        key={key}
-                        name={key}
-                        keys={[rootName, key]} // Start with rootName and add the current key
-                        value={value}
-                        expandLevel={expandLevel - 1}
-                        onCopy={onCopy}
-                      />
-                    ))
-                  : null}
-              </div>
-            )}
-            {!collapsed && (
-              <span
-                tabIndex={0}
-                role='button'
-                aria-label={`Collapse ${rootName}`}
-                onClick={toggleCollapse}
-                onKeyDown={(e: KeyboardEvent<HTMLDivElement>) =>
-                  handleKeyDown(e, toggleCollapse)
-                }
-                className={keyClass}
-              >
-                {closingBracket}
-              </span>
-            )}
-          </>
-        ) : (
-          <>
-            <span className={keyClass}>{rootName}: </span>
-            <PrimitiveValue value={data as Primitive} />
-          </>
-        )}
-      </div>
+      <CollapsibleProvider>
+        <JsonViewerContent
+          data={data}
+          rootName={rootName}
+          style={style}
+          expandLevel={expandLevel}
+          onCopy={onCopy}
+        />
+      </CollapsibleProvider>
     );
   }
 );
