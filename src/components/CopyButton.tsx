7504685bc88e8a781af11abe223298e1fd772f6b
Ruslan Shulga <nycruslan@Ruslans-MacBook-Pro.local>
2024-02-17 19:38:45 -0500

    copy button improved

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import styles from '../styles.module.scss';
 
 export const CopyButton = ({ handleCopy }: { handleCopy: () => void }) => {
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  const handleClick = () => {
+    handleCopy();
+    setCopySuccess(true);
+    setTimeout(() => setCopySuccess(false), 1000); // Reset after 1s
+  };
+
   return (
-    <button onClick={handleCopy} className={styles.copyButton}>
-      Copy
+    <button
+      onClick={handleClick}
+      className={`${styles.copyButton} ${
+        copySuccess ? styles.copySuccess : ''
+      }`}
+    >
+      copy
     </button>
   );
 };
